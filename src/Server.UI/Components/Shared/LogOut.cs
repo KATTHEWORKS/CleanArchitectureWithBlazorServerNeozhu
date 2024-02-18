5f9ebdaf230c0be1763bd931b92a05d5c211f6ad
nammadhu@KiSS <madhusudhan.v@live.in>
2024-02-18 18:16:08 +0530

    after voting application gets loggedout

--- src/Server.UI/Components/Shared/LogOut.cs
+++ src/Server.UI/Components/Shared/LogOut.cs
@@ -8,24 +8,27 @@ namespace CleanArchitecture.Blazor.Server.UI.Components.Shared;
 
 public interface ILogOut
 {
-    Task Execute();
+    Task Execute(string? message = null, bool force = false);
 }
 
 public class LogOut(IDialogService dialogService, IAccessTokenProvider tokenProvider, AuthenticationStateProvider authenticationStateProvider) : ILogOut
 {
-    public async Task Execute()
+    public async Task Execute(string? message = null, bool force = false)
     {
+        var confirmed = force;
+
         var parameters = new DialogParameters<LogoutConfirmation>
         {
-            { x => x.ContentText, $"{ConstantString.LogoutConfirmation}" },
+            { x => x.ContentText,force?"After changes you must logout to see changes": $"{ConstantString.LogoutConfirmation},{message??""}" },
             { x => x.Color, Color.Error }
         };
 
         var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.ExtraSmall, FullWidth = true };
         var dialog = dialogService.Show<LogoutConfirmation>(ConstantString.LogoutNecessaryConfirmationTitle, parameters, options);
         var result = await dialog.Result;
+        confirmed = force||!result.Canceled;
 
-        if (!result.Canceled)
+        if (confirmed)
         {
             await tokenProvider.RemoveAuthDataFromStorage();
             ((BlazorAuthenticationStateProvider)authenticationStateProvider).MarkUserAsLoggedOut();
