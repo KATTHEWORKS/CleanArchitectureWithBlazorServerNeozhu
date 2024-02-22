453c2bf39b7d52ee1ca8824b5e084fb4c170ca48
nammadhu@KiSS <madhusudhan.v@live.in>
2024-02-22 21:58:37 +0530

    vote and summary entity correction

--- src/Application/Features/VotingSystem/Votes/Commands/AddEdit/AddEditVoteCommand.cs
+++ src/Application/Features/VotingSystem/Votes/Commands/AddEdit/AddEditVoteCommand.cs
@@ -6,20 +6,20 @@ using CleanArchitecture.Blazor.Application.Features.VotingSystem.Votes.Caching;
 using CleanArchitecture.Blazor.Domain.Events.VotingSystem;
 namespace CleanArchitecture.Blazor.Application.Features.VotingSystem.Votes.Commands.AddEdit;
 
-public class AddEditVoteCommand: ICacheInvalidatorRequest<Result<int>>
+public class AddEditVoteCommand : ICacheInvalidatorRequest<Result<int>>
 {
-      [Description("Id")]
-      public int Id { get; set; }
-    
+    [Description("Id")]
+    public int Id { get; set; }
+
     [Description("User Id")]
     public string? UserId { get; set; }//not in UI,set from logged in
 
     [Description("Constituency Id")]
     public int ConstituencyId { get; set; } //not in UI,set from selected constituency
-    
-    
+
+
     [Description("Open Issues/Problems Needs to be addressed")]
-    public string? OpenIssues {get;set;}
+    public string? OpenIssues { get; set; }
     //public sbyte Rating { get;set  }//auto-generate based on kpi values//make it readonly from user but 
 
     [Description("Do you wish to Re-Elect Current MP again?")]
@@ -31,55 +31,55 @@ public class AddEditVoteCommand: ICacheInvalidatorRequest<Result<int>>
     public DateTime? LastModified { get; set; }//for display only
 
     public string CacheKey => VoteCacheKey.GetAllCacheKey;
-      public CancellationTokenSource? SharedExpiryTokenSource => VoteCacheKey.SharedExpiryTokenSource();
+    public CancellationTokenSource? SharedExpiryTokenSource => VoteCacheKey.SharedExpiryTokenSource();
 
     private class Mapping : Profile
     {
         public Mapping()
         {
-            CreateMap<VoteDto,AddEditVoteCommand>(MemberList.None);
-            CreateMap<AddEditVoteCommand,Vote>(MemberList.None);
-         
+            CreateMap<VoteDto, AddEditVoteCommand>(MemberList.None);
+            CreateMap<AddEditVoteCommand, Vote>(MemberList.None);
+
         }
     }
 }
 
-    public class AddEditVoteCommandHandler : IRequestHandler<AddEditVoteCommand, Result<int>>
+public class AddEditVoteCommandHandler : IRequestHandler<AddEditVoteCommand, Result<int>>
+{
+    private readonly IApplicationDbContext _context;
+    private readonly IMapper _mapper;
+    private readonly IStringLocalizer<AddEditVoteCommandHandler> _localizer;
+    public AddEditVoteCommandHandler(
+        IApplicationDbContext context,
+        IStringLocalizer<AddEditVoteCommandHandler> localizer,
+        IMapper mapper
+        )
     {
-        private readonly IApplicationDbContext _context;
-        private readonly IMapper _mapper;
-        private readonly IStringLocalizer<AddEditVoteCommandHandler> _localizer;
-        public AddEditVoteCommandHandler(
-            IApplicationDbContext context,
-            IStringLocalizer<AddEditVoteCommandHandler> localizer,
-            IMapper mapper
-            )
-        {
-            _context = context;
-            _localizer = localizer;
-            _mapper = mapper;
-        }
-        public async Task<Result<int>> Handle(AddEditVoteCommand request, CancellationToken cancellationToken)
-        {
-            if (request.Id > 0)
-            {
-                var item = await _context.Votes.FindAsync(new object[] { request.Id }, cancellationToken) ?? throw new NotFoundException($"Vote with id: [{request.Id}] not found.");
-                item = _mapper.Map(request, item);
-				// raise a update domain event
-				item.AddDomainEvent(new VoteUpdatedEvent(item));
-                await _context.SaveChangesAsync(cancellationToken);
-                return await Result<int>.SuccessAsync(item.Id);
-            }
-            else
-            {
-                var item = _mapper.Map<Vote>(request);
-                // raise a create domain event
-				item.AddDomainEvent(new VoteCreatedEvent(item));
-                _context.Votes.Add(item);
-                await _context.SaveChangesAsync(cancellationToken);
-                return await Result<int>.SuccessAsync(item.Id);
-            }
-           
-        }
+        _context = context;
+        _localizer = localizer;
+        _mapper = mapper;
     }
+    public async Task<Result<int>> Handle(AddEditVoteCommand request, CancellationToken cancellationToken)
+    {
+        if (request.Id > 0)
+        {
+            var item = await _context.Votes.FindAsync(new object[] { request.Id }, cancellationToken) ?? throw new NotFoundException($"Vote with id: [{request.Id}] not found.");
+            item = _mapper.Map(request, item);
+            // raise a update domain event
+            item.AddDomainEvent(new VoteUpdatedEvent(item));
+            await _context.SaveChangesAsync(cancellationToken);
+            return await Result<int>.SuccessAsync(item.Id);
+        }
+        else
+        {
+            var item = _mapper.Map<Vote>(request);
+            // raise a create domain event
+            item.AddDomainEvent(new VoteCreatedEvent(item));
+            _context.Votes.Add(item);
+            await _context.SaveChangesAsync(cancellationToken);
+            return await Result<int>.SuccessAsync(item.Id);
+        }
+
+    }
+}
 
---
+++ src/Migrators/Migrators.MSSQL/Migrations/20240222162536_voteAndSummaryCorrection.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Blazor.Migrators.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class voteAndSummaryCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WriteCount",
+                table: "Constituencies");
+
+            migrationBuilder.AddColumn<short>(
+                name: "Rating",
+                table: "VoteSummaries",
+                type: "smallint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VoteCount",
+                table: "VoteSummaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VoteCountAgainstExistingMp",
+                table: "VoteSummaries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "VoteCountForExistingMp",
+                table: "VoteSummaries",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OpenIssues",
+                table: "Votes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "WishToReElectMp",
+                table: "Votes",
+                type: "bit",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "VoteSummaries");
+
+            migrationBuilder.DropColumn(
+                name: "VoteCount",
+                table: "VoteSummaries");
+
+            migrationBuilder.DropColumn(
+                name: "VoteCountAgainstExistingMp",
+                table: "VoteSummaries");
+
+            migrationBuilder.DropColumn(
+                name: "VoteCountForExistingMp",
+                table: "VoteSummaries");
+
+            migrationBuilder.DropColumn(
+                name: "OpenIssues",
+                table: "Votes");
+
+            migrationBuilder.DropColumn(
+                name: "WishToReElectMp",
+                table: "Votes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "WriteCount",
+                table: "Constituencies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
