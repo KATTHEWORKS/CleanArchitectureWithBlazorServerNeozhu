24138a94e9660ffa34cd9e3bb71b24aa743d1be9
nammadhu@KiSS <madhusudhan.v@live.in>
2024-04-27 17:37:04 +0530

    c

--- src/Presentation/Presentation.ClientWasm/Program.cs
+++ src/Presentation/Presentation.ClientWasm/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace Presentation.ClientWasm;
@@ -7,7 +7,7 @@ public class Program
     public static async Task Main(string[] args)
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
+        //builder.RootComponents.Add<AppNotUsed>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
